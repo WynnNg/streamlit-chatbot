ce20664b48f13958a8b1c5d07eef773a8008db69
Nguyễn Hải Phúc Nguyên <nguyenhaiphucnguyen@gmail.com>
2025-05-30 23:12:53 +0700

    First Commit

---
+++ pages/Chat.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import requests
+import uuid
+
+BACKEND_URL_API = st.secrets['BACKEND_URL_API']
+
+def clear_session_state():
+    for key in st.session_state.keys():
+        del st.session_state[key]
+
+st.header("Chatbot")
+
+if "backend_chat_api" not in st.session_state:
+    st.session_state.backend_chat_api = "{}/chat".format(BACKEND_URL_API)
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+
+for message in st.session_state.chat_history:
+    role = message["role"]
+    content = message["content"]
+    with st.chat_message(role):
+        st.markdown(content) 
+
+query = st.chat_input(f"Dạ anh/chị cần tư vấn gì ạ?: {st.session_state.backend_chat_api}")
+
+if query:
+
+    with st.chat_message("user"):
+        st.markdown(query)
+
+    user_response = {
+        "role" : "user",
+        "content": query
+    }
+    st.session_state.chat_history.append(user_response)
+
+    with st.chat_message("assistant"):
+        payload = [{
+			"role": "user",
+			"parts": [
+				{
+					"text": query
+				}
+			]
+		}]
+
+        response = requests.post(st.session_state.backend_chat_api, json = payload)
+
+        if response.status_code == 200:
+            api_response = response.json()
+            assistant_msg = api_response["parts"][0]["text"]
+            assistant_response = {
+                "role" : "assistant",
+                "content": assistant_msg
+            }
+            st.session_state.chat_history.append(assistant_response)
+            st.markdown(assistant_msg)
+        else:
+            st.error(f"Error: {response.status_code}")
+
+
+    
